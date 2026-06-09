a34fa59c35a61262cdd8e02992c9a687a6121295
cristianledesma2512-a11y <cristianledesma2512@gmail.com>
2026-06-09 08:40:38 -0300

    Update scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -392,7 +392,7 @@ CANALES_FIJOS = [
         "id": "cap01",
         "nombre": "canal1",
         "categoria": "DEPORTES",
-        "url": "<iframe width="1340" height="754" src="https://www.youtube.com/embed/cb12KmMMDJA" title="TN EN VIVO - SEGUÍ LA TRANSMISIÓN EN VIVO DE TODO NOTICIAS" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>",
+        "url": "https://latamvidz1.com/canal.php?stream=dsports",
         "logo": "https://cdn-icons-png.flaticon.com/512/53/53283.png",
         "fallbacks": []
       },
